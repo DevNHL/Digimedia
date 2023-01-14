572b4e72cbf7084bfebfe6ce71ca82d2b35fd150
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-01-14 13:15:52 -0500

    cambios

--- index.php
+++ index.php
@@ -15,68 +15,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="css/stylos.css?=241222">
+    <link rel="stylesheet" href="css/stylos.css">
 
 </head>
 
 <body>
-    <header class="bg-dark sticky-top">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html">
-                        <img src="img/logo.png" alt="Logo">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <!-- <ul class="navbar-nav ms-auto">
-                            <form class="d-flex" role="search">
-                                <input class="form-control me-2 mb-2 mt-2" type="search"
-                                    placeholder="¿Que estas buscando?" aria-label="Search">
-                                <button class="btn btn-outline-success mb-2 mt-2" type="submit"><i
-                                        class="fa-solid fa-magnifying-glass"></i></button>
-                            </form>
-                        </ul> -->
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a class="nav-link hv active" aria-current="page" href="index.html">Inicio</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link hv dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Servicios
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="mk-gd.html">Marketing Digital</a></li>
-                                    <li><a class="dropdown-item" href="img-corp.html">Imagen Corporativa</a></li>
-                                    <li><a class="dropdown-item" href="rd-sociales.html">Redes Sociales</a></li>
-                                    <li><a class="dropdown-item" href="marca.html">Crea tu marca</a></li>
-                                    <li><a class="dropdown-item" href="ds-web.html">Dise&ntilde;o Web</a></li>
-                                    <li><a class="dropdown-item" href="ds-grafico.html">Dise&ntilde;o Gr&aacute;fico</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link hv" href="blog.html">Blog</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link hv" href="contacto.html">Cont&aacute;ctanos</a>
-                            </li>
-                        </ul>
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="preguntas.html"><i class="fa-solid fa-comments"></i></a>
-                            </li>
-                        </ul>
-                        
-                    </div>
-            </nav>
-        </div>
-    </header>
-
+    
     <main class="pt-5">
         <div class="container">
             <section class="row">
