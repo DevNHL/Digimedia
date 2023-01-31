34ceece0f58e20394571f4cf682ba2874de91738
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-01-31 13:03:39 -0500

    cambio

--- View/contacto.php
+++ View/contacto.php
@@ -11,6 +11,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
     <link rel="stylesheet" href="../css/stylos.css">
+    <script type="text/javascript">
+    function valideKey(evt) {
+
+        var code = (evt.which) ? evt.which : evt.keyCode;
+
+        if (code == 8) {
+            return true;
+        } else if (code >= 48 && code <= 57) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+</script>
+
 </head>
 
 <body>
@@ -75,19 +90,5 @@
 <script src="../js/letra.js"></script>
 <script src="../js/formulalriovalidasegundo.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-<script type="text/javascript">
-    function valideKey(evt) {
-
-        var code = (evt.which) ? evt.which : evt.keyCode;
-
-        if (code == 8) {
-            return true;
-        } else if (code >= 48 && code <= 57) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-</script>
 
 </html>
