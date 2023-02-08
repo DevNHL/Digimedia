04728faed906bbcd9d234a75f24e0c8423ee5029
Jennykings <jennykings51@gmail.com>
2023-02-08 14:08:53 -0500

    añadiendo estilos faltantes

--- View/preguntas.php
+++ View/preguntas.php
@@ -13,6 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
     <link rel="stylesheet" href="css/stylos.css">
+    <link rel="stylesheet" href="../css/header.css">
 
 </head>
 
