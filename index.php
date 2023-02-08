6712a6cc1c0fcf43973cfe657e69c63408c9b164
Jennykings <jennykings51@gmail.com>
2023-02-08 17:55:34 -0500

    añadiendo div

--- index.php
+++ index.php
@@ -34,7 +34,6 @@
                     <img loading="lazy" class="img-fluid" src="img/section.webp" alt="reunion">
                 </article>
             </section>
-
             <section class="row pb-5" id="servicios">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <div class="col">
@@ -126,7 +125,7 @@
                 </div>
             </div>
         </div>
-
+     </div>
 </body>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
