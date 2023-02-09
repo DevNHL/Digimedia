a5ca85fe800eb0846b9026f4ce32f185de4366e6
Jennykings <jennykings51@gmail.com>
2023-02-09 14:11:27 -0500

    ordenando scripts

--- View/contacto.php
+++ View/contacto.php
@@ -90,8 +90,8 @@
     <?php include '../assets/footer.html' ?>
 </body>
 <!--scripts-->
-<script src="../js/letra.js"></script>
+<!-- <script src="../js/letra.js"></script>
 <script src="../js/formulalriovalidasegundo.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> -->
 
 </html>
