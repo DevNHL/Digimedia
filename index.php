76f5500b6bdda660002d8ee2913d1aa9dcd48128
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-02-09 19:29:02 -0500

    index

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>.:: DigiMedia ::.</title>
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Stylos Css propios -->
+    <link rel="stylesheet" href="css/stylos.css">
+
+</head>
+
+<body>
+    <?php include 'assets/header.html' ?>
+    <main class="pt-5">
+        <div class="container">
+            <section class="row bg-white">
+                <article class="col-12 col-lg-5">
+                    <h2 class="fw-bold">¿Quienes somos?</h2>
+                    <hr>
+                    <div class="fs-5">
+                        Digimedia es una empresa de marketing digital, que se enfoca en potenciar tu emprendimiento a
+                        nivel online. Digimedia, le brinda a tu emprendimiento estrategias que ayuden a cumplir los
+                        objetivos de manera eficaz. Somos un grupo de personas comprometidas con el desarrollo de cada
+                        marca que nos contacta.
+                    </div>
+                </article>
+                <article class="col-12 col-lg-7">
+                    <img loading="lazy" class="img-fluid" src="img/section.webp" alt="reunion">
+                </article>
+            </section>
+
+            <section class="row pb-5" id="servicios">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <img loading="lazy" src="img/marketing.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Marketing Digital</h5>
+                                <p class="card-text">En Digimedia te ayudamos con el desarrollo de tu página web, con una estrategia de marketing digital que encaje con el rubro de tu negocio y los objetivos de este, con el posicionamiento en el mundo online, y con el control que debes tener en tus redes sociales. </p>
+                                <a href="View/mk-gd.php" class="btn btn-primary">Mas informaci&oacute;n</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img loading="lazy" src="img/img-corp.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Imagen Corporativa</h5>
+                                <p class="card-text">En Digimedia te ayudamos con el desarrollo de tu página web, con una estrategia de marketing digital que encaje con el rubro de tu negocio y los objetivos de este, con el posicionamiento en el mundo online, y con el control que debes tener en tus redes sociales. </p>
+                                <a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdropss">Mas informaci&oacute;n </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img loading="lazy" src="img/marca.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Gesti&oacute;n de Redes Sociales</h5>
+                                <p class="card-text">En Digimedia te ayudamos con el desarrollo de tu página web, con una estrategia de marketing digital que encaje con el rubro de tu negocio y los objetivos de este, con el posicionamiento en el mundo online, y con el control que debes tener en tus redes sociales. </p>
+                                <a href="View/rd-sociales.php" class="btn btn-primary">Mas informaci&oacute;n </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img loading="lazy" src="img/marca.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Crea tu marca</h5>
+                                <p class="card-text">En Digimedia te ayudamos a desarrollar tu marca desde cero, único y diferenciado para que tenga las máximas garantías de éxito, construimos la identidad visual de tu marca con el objetivo de proporcionarle alma propia, despertar sensaciones y crear conexiones</p>
+                                <a href="View/marca.php" class="btn btn-primary">Mas informaci&oacute;n </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img loading="lazy" src="img/web.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Dise&ntilde;o Web</h5>
+                                <p class="card-text">En Digimedia te ayudamos con el desarrollo del diseño web de tu marca. Digimedia, se especializa en el diseño de sitios web modernos, fáciles de usar y que reflejen la marca de su empresa. Trabajamos con usted para entender su negocio y lo que quiere lograr con su sitio web.
+                                </p>
+                                <a href="View/ds-web.php" class="btn btn-primary">Mas informaci&oacute;n</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img loading="lazy" src="img/grafico.webp" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Dise&ntilde;o Gr&aacute;fico</h5>
+                                <p class="card-text ">En Digimedia te ayudamos y realizamos coaching en el diseño gráfico de las piezas publicitarias. Nuestro servicio de diseño gráfico publicitario y digital, se centra en buscar crear piezas que vallan acorde con la personalidad y el mensaje de tu marca.</p><br>
+                                <a href="View/ds-grafico.php" class="btn btn-primary">Mas informaci&oacute;n </a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <?php include 'assets/footer.html' ?>
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdropss" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea velit rerum saepe dolorum sint perferendis, ex animi minima quia qui id illum enim, hic harum quod non ratione incidunt molestiae?
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum alias quam pariatur veniam aliquam voluptas, fugiat tempore possimus. Nostrum fuga natus dignissimos possimus officiis laudantium cumque eum, necessitatibus facere animi.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi repudiandae neque perspiciatis sed quod consequuntur culpa consectetur? Dignissimos, accusamus quam, dolore laborum quae id eius quasi officia pariatur deserunt minima.
+                    Asperiores labore aut tempore quas natus, numquam nesciunt laboriosam nostrum optio impedit commodi deleniti fugit, architecto quaerat aliquid eius recusandae exercitationem! Iusto, consequuntur. Ipsam architecto similique corporis iure repudiandae quod.
+                    lorem*2
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+            </div>
+        </div>
+
+</body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
+</html>
