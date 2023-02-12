92fb6e44b7e806084533d27124e3f3d01531d455
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-02-12 18:43:25 -0500

    update

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="css/stylos.css?120223">
+    <link rel="stylesheet" href="css/stylos.css?120224">
     <link rel="stylesheet" href="css/carrosel.css">
 </head>
 
@@ -43,7 +43,7 @@
     </div>
     <div class="pt-4"></div>
     <div class="container">
-        <h4 class="letra-sub">Servicios para tu Marca</h4>
+        <h4 class="letra-sub">Servicios de Marketing</h4>
     </div>
     <main class="container">
         <!--carrosel-->
