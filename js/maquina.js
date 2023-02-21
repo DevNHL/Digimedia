4ad7fd80fb9c84c6c6a7d40b3adb9ded8e39c636
Jennykings <jennykings51@gmail.com>
2023-02-21 17:11:53 -0500

    prueba

--- js/maquina.js
+++ js/maquina.js
@@ -21,4 +21,29 @@ const maquinaEscribir = (text = '',tiempo = 200, etiqueta = '') => {
     }, tiempo)
   }
   
-  maquinaEscribir("DISEÑO WEB . ",100, maquina)
+  maquinaEscribir("GESTIÓN DE REDES  SOCIALES.  ",100, maquina)
+
+  const maquina1=document.getElementById("maquina1")
+
+const maquinaEscribir1 = (text = '',tiempo = 200, etiqueta = '') => {
+    let arrayCaracteres = text.split('')
+    etiqueta.innerHTML = ''
+    let i = 0
+    let j = text.length
+    let escribir = setInterval(function(){
+      if (i === arrayCaracteres.length) {
+        etiqueta.innerHTML = text.substring(0,j)
+        j--
+        if (j === 0) {
+          etiqueta.innerHTML = ''
+          i = 0
+          j = text.length
+        }
+      } else {
+        etiqueta.innerHTML += arrayCaracteres[i]
+        i++
+      }
+    }, tiempo)
+  }
+  
+  maquinaEscribir1("DISEÑO WEB. ",100, maquina1)
