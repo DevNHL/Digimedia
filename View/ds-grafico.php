3defaca8bde28a11a85cd5ee906561a5e29606ba
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-02-20 22:04:16 -0500

    cambios a medios

--- View/ds-grafico.php
+++ View/ds-grafico.php
@@ -12,14 +12,26 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="css/stylos.css">
+    <link rel="stylesheet" href="../css/diseño _grafico.css">
 
 </head>
 
 <body>
-    <?php include '../assets/header_view.html' ?>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <?php include '../assets/footer.html' ?>
+    <?php include 'header.html' ?>
+    <div class="banner">
+        <div class="con">
+            <div class="contenudi">
+                <div class="textos">
+                    <span class="text">DISEÑO GR&Aacute;FICO</span>    
+                    <p class="parrafo">Con nuestro servicio de Diseño Gráfico, crearemos piezas que logren transmitir la personalidad <br>
+                        de tu marca. A través de ideas y mensajes creativos, lograremos darle poder a tu marca.
+                    </p>
+                    <a href="contacto.php" class="btnx">Contactar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.html' ?>
 
 </body>
 <!-- JavaScript Bundle with Popper -->
