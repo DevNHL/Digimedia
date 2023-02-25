a9b532fa766f5cc8a601cdfcb28d65de3f152a2b
Jennykings <jennykings51@gmail.com>
2023-02-25 11:13:47 -0500

    aagregando

--- View/marca.php
+++ View/marca.php
@@ -22,7 +22,7 @@
         <div class="padre">
         <div class="contenedors">
             <div class="ckjb">
-                <div class="tit" id="maquina1">CREA TU MARCA.</div>
+                <div class="tit" id="maquina">CREA TU MARCA.</div>
             </div>
             
             <p class="sub">En Digimedia ayudaremos a mejorar la imagen de tu marca. Asimismo, a incrementar y conquistar <br>   
