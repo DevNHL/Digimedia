7ff5e9cf595ebaa62e85e99224917daaf466b84f
Jennykings <jennykings51@gmail.com>
2023-03-06 23:10:06 -0500

    añadiendo contenido a pagina

--- View/marketing-digital.php
+++ View/marketing-digital.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
     <!-- <link rel="stylesheet" href="../css/dw.css?2002e23"> -->
-    <link rel="stylesheet" href="../css/mk_digital.css">
+    <link rel="stylesheet" href="../css/mk.css">
 
 </head>
 
@@ -25,9 +25,8 @@
             <div class="tit" id="maquina">MARKETING DIGITAL.</div>
             </div>
             
-            <p class="sub">En Digimedia, construimos contigo la presencia de tu empresa en internet. Enfocados en el <br>
-            marketing digital, buscamos que tu web esté enfocada en tus visitantes y optimizada para <br>
-            Google (SEO).</p>
+            <p class="sub">En Digimedia, te ayudamos con el posicionamiento en el mundo online, <br>
+            y con el control que debes tener en tus redes sociales.</p>
             <div class="con">
                 <a href="contacto.php" class="boton_con">Contactar</a>
             </div>
@@ -40,9 +39,8 @@
     <div class="bannersec">
         <div class="conrs">
             <div class="cont">
-                <div class="titulo"><p class="tes">Servicio de Diseño Web</p></div>
-                <p class="parrafo">Implementar tu plataforma web para tu negocio ayudará,a que muchos procesos internos y externos sean optimizados. <br>
-                Empieza con tu plan de diseño de páginas web y dale a tu marca un lugar único en internet para llegar a más personas y brindarles una experiencia cómoda, amigable y sorprendente. 
+                <div class="titulo"><p class="tes">Servicios de Marketing Digital</p></div>
+                <p class="parrafo">Seremos parte del crecimiento de tu marca, identificando, proyectando, realizando y midiendo las estrategias de marketing digital para lograr todos sus objetivos.
                 </p>
             </div> 
         </div>
@@ -53,18 +51,9 @@
 
     <div class="bannersec3">
         <div class="conrs2">
-            <div class="cont2">
-                <div class="titulo2"><p class="tes">Diseño de página web en Lima - Perú</p></div>
-                <p class="parrafo">Crear tu página web te permite transmitir una correcta información sobre los productos o servicios de tu marca basada en la experiencia de usuario (UX). Nuestro servicio incluye:<br>
-                     <ul class="parrafo">
-                         <li type="disc">Análisis de competencia </li>
-                         <li type="disc">UX/UI </li>
-                         <li type="disc">Diseño Responsive .</li>
-                         <li type="disc">Chat integrado  </li>
-                         <li type="disc">Sitio web administrable </li>
-                         <li type="disc">Data Estructurada </li>
-                         <li type="disc">Optimización SEO</li>
-                     </ul>
+            <div class="cont">
+                <div class="titulo2"><p class="tes">Creamos tu estrategia de Marketing Digital</p></div>
+                <p class="parrafo">Nos encargamos de realizar las estrategias de Marketing Digital para aumentar presencia y relevancia en medios digitales como redes sociales, sitios web o incluso dentro de los motores de búsqueda como Google.
                 </p>
             </div> 
         </div>
@@ -75,16 +64,9 @@
     <div class="bannersec4">
         <div class="conrs">
             <div class="cont">
-                <div class="titulo"><p class="tes">Nuestra técnica de trabajo</p></div>
-                <p class="parrafo">En Digimedia disponemos de un equipo de profesionales desarrolladores , los cuales se enfocaran en la planificación y desarrollo de páginas web, con la finalidad de responder a las necesidades de la empresa y la de los usuarios.<br>
-                     <ul class="parrafo">
-                         <li type="disc">Análisis e investigación</li>
-                         <li type="disc">Experiencia de usuario y diseño de interfaces</li>
-                         <li type="disc">Ejecución de desarrollo web</li>
-                         <li type="disc">User testing</li>
-                         <li type="disc">Optimización del sitio web (SEO on page)</li>
-                         <li type="disc">Entrega del sitio web</li>
-                     </ul>
+                <div class="titulo"><p class="tes">Plan de Marketing Digital</p></div>
+                <p class="parrafo">Desarrollamos planes de marketing digital para implementar una estrategia de SEO, SEM o redes sociales que te permitan alcanzar tus objetivos de marca y ventas. <br> <br>
+                    Si tu empresa es nueva en el mundo digital o  no consigues los resultados deseables de tu actual estrategia digital, contáctanos.
                 </p>
             </div> 
         </div>
