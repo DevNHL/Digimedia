c1b628a5b1933d43cdf7380d4a6fa04134a692a3
Jeam06 <jesusangeldonismaslucan@gmail.com>
2023-03-09 14:00:29 -0500

    cambios y correcciones

--- View/contacto.php
+++ View/contacto.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>.:: DigiMedia ::.</title>
-    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
@@ -17,11 +17,19 @@
 
 <body>
     <?php include 'header.html' ?>
-        <img src="/img/contacto.png" width="100%" height="auto"></img>
+    <div class="contactanos-image " id="banner">
+        <div class="sombra">
+        </div>
+        <div class="letra" id="">
+            <div class="borde" id="digimedia">
+                <p class="" id=""><u class="delgado">CONTÁCTANOS</u></p>
+            </div>
+        </div>
+    </div>
         <div class="pt-5"></div>
             <div class="container">
-                <div class="col-md-8 offset-md-2">
-                <form id="formulario" class="card cards2" action="blog.php">
+                <div class="col-md-10 offset-md-1">
+                <form id="formulario" class="" action="blog.php">
                     
                         <div class="row">
                             <div class="col-md-12">
@@ -34,7 +42,7 @@
                     <div class="row">    
                         <div class="col-md-6">
                         <div class="mb-3">
-                            <div class="pt-3"></div>
+                            <div class="pt-3">Nombre</div>
                             <input type="text" class="form-control" id="nombre" placeholder="Nombres y Apellidos">
                             <small></small>
                         </div>
@@ -42,7 +50,7 @@
 
                         <div class="col-md-6">
                         <div class="mb-3">
-                            <div class="pt-3"></div>
+                            <div class="pt-3">Email</div>
                             <input type="email" class="form-control" id="email" placeholder="email">
                             <small></small>
                         </div>
@@ -52,7 +60,7 @@
                     <div class="row">
                         <div class="col-md-6">
                         <div class="mb-3">
-                            <div class="pt-3"></div>
+                            <div class="pt-3">Servicio</div>
                             <select class="form-control" id="servicio">
                                 <option hidden>--Servicio--</option>
                                 <option value="#">1</option>
@@ -64,7 +72,7 @@
 
                         <div class="col-md-6">
                         <div class="mb-3">
-                            <div class="pt-3"></div>
+                            <div class="pt-3">Numero</div>
                             <input type="text" class="form-control" id="telefono" onkeypress="return valideKey(event);" placeholder="Telefono">
                             <small></small>
                         </div>
@@ -72,7 +80,7 @@
                     </div>
 
                         <div class="mb-3">
-                            <div class="pt-3"></div>
+                            <div class="pt-3">Mensaje</div>
                             <textarea name="mensajes" class="form-control" id="mensaje" cols="30" rows="4" placeholder="Mensaje"></textarea>
                             <small></small>
                         </div>
