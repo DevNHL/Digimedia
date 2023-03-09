269b23a4dc7b825ad2ca9cf3e922ab2e61a6f05e
DanniloTorres <torresdannilo07@gmail.com>
2023-03-09 13:33:47 -0500

    pagina-Nosotros
    dany-res

--- View/nosotros.php
+++ View/nosotros.php
@@ -12,15 +12,87 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="css/stylos.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/stylos.css">
+    <link rel="stylesheet" href="../css/nosotros.css">
+
+ 
 </head>
 
 <body>
     <?php include 'header.html' ?>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    
+    <div class="banerprincipal">
+  <div class="padre">
+    <div class="contenedors">
+      <div class="ckjb">
+        <div class="tit" id="maquinas">NOSOTROS</div>
+      </div>         
+    </div>
+  </div>
+</div>
+<div class="pt-5"></div>
+
+<div class="banner">
+  <div class="content">
+    <h1>NOSOTROS</h1>
+    <p>Digimedia es una empresa de marketing digital, que se enfoca en potenciar tu emprendimiento a nivel online. Digimedia, le brinda a tu emprendimiento estrategias que ayuden a cumplir los objetivos de manera eficaz. Somos un grupo de personas comprometidas con el desarrollo de cada marca que nos contacta.</p>
+  </div>
+  <img src="../img/Nos_imagen2.webp" alt="Descripción de la imagen">
+</div>
+<div class="pt-5"></div>
+
+<div class="container2">
+  <div class="left">
+    <h2><img src="../img/mirador.svg" alt="Icono" class="icono">Misión</h2>
+    <p>Ser aliado de los emprendimientos en su posicionamiento digital; mediante la generación de contenido estratégico que garantice el cumplimiento de los objetivos planteados.</p>
+  </div>
+  <div class="right">
+    <h2><img src="../img/vision.svg" alt="Icono" class="icono">Visión</h2>
+    <p>Liderar la transformación digital de las PYME en el Perú; estableciendo vínculos sólidos entre nuestros clientes y sus respectivas audiencias.</p>
+  </div>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<div class="pt-5"></div>
+<div class="container">
+  <div class="titulo2">
+    <h1><img src="../img/brazos.svg" alt="Icono" class="icono">NUESTROS VALORES</h1>
+  </div>
+</div>
+
+<div class="texto-lin">
+  <p>En nuestra DigiMedia, creemos que nuestros valores son la clave de nuestro éxito y nos guían en todas nuestras decisiones y acciones.</p>
+</div>
+
+<div class="contenedor9">
+  <div class="items">
+    <h2>Responsabilidad</h2>
+    <p>Nos esforzamos por ser responsables con nuestros clientes, nuestro personal y nuestro medio ambiente.</p>
+  </div>
+  <div class="items">
+    <h2>Innovación</h2>
+    <p>Creemos en la importancia de pensar fuera de la caja y ser pioneros en la implementación de soluciones innovadoras.</p>
+  </div>
+  <div class="items">
+    <h2>Adaptabilidad</h2>
+    <p>Somos flexibles y estamos dispuestos a aprender y cambiar para tener éxito en un entorno en constante cambio.</p>
+  </div>
+  <div class="items">
+    <h2>Trabajo en equipo</h2>
+    <p>Fomentamos un ambiente de colaboración y apoyo en el que todos puedan prosperar y alcanzar su máximo potencial.</p>
+  </div>
+  <div class="items">
+    <h2>Compromiso</h2>
+    <h2></h2>
+    <p>Estamos comprometidos con nuestro trabajo y con lograr un impacto positivo en la comunidad.</p>
+  </div>
+</div>
+
+
+
+
+
+    <br><br><br>
     <?php include 'footer.html' ?>
 </body>
-
+<script src="../js/nosodi.js"></script> 
 </html>
