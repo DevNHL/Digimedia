d53309519c14e81dff106f273602e9fed37ef8bf
Jennykings <jennykings51@gmail.com>
2023-03-10 15:39:40 -0500

    añadiendo contacto

--- View/contacto.php
+++ View/contacto.php
@@ -17,11 +17,20 @@
 
 <body>
     <?php include 'header.html' ?>
-    <div class="pt-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 offset-md-2">
-                <form id="formulario" class="card cards2" action="blog.php">
+    <div class="contact" id="banner">
+        <div class="sombra">
+        </div>
+        <div class="letra" id="">
+            <div class="borde" id="digimedia">
+                <p class="" id=""><u class="delgado">CONTÁCTANOS</u></p>
+            </div>
+        </div>
+    </div>
+        <div class="pt-5"></div>
+            <div class="container">
+                <div class="col-md-10 offset-md-1">
+                <form id="formulario" class="" action="blog.php">
+                    
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="alert alert-warning  ale  text-center" role="alert" id="alert">
@@ -29,38 +38,68 @@
                                 </div>
                             </div>
                         </div>
-                        <div class=" mb-3">
-                            <div class="pt-3"></div>
+
+                    <div class="row">    
+                        <div class="col-md-6">
+                        <div class="mb-3">
+                            <div class="pt-3">Nombre</div>
                             <input type="text" class="form-control" id="nombre" placeholder="Nombres y Apellidos">
+                            <small></small>
+                        </div>
+                        </div>
+
+                        <div class="col-md-6">
+                        <div class="mb-3">
+                            <div class="pt-3">Email</div>
+                            <input type="email" class="form-control" id="email" placeholder="email">
+                            <small></small>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                        <div class="mb-3">
+                            <div class="pt-3">Servicio</div>
+                            <select class="form-control" id="servicio">
+                                <option hidden>--Servicio--</option>
+                                <option value="#">1</option>
+                                <option value="#">2</option>
+                            </select>
+                            <small></small>
+                        </div>
+                        </div>
+
+                        <div class="col-md-6">
+                        <div class="mb-3">
+                            <div class="pt-3">Numero</div>
+                            <input type="text" class="form-control" id="telefono" onkeypress="return valideKey(event);" placeholder="Telefono">
+                            <small></small>
+                        </div>
+                        </div>
+                    </div>
+
+                        <div class="mb-3">
+                            <div class="pt-3">Mensaje</div>
+                            <textarea name="mensajes" class="form-control" id="mensaje" cols="30" rows="4" placeholder="Mensaje"></textarea>
                             <small></small>
                         </div>
                         <div class="mb-3">
                             <div class="pt-3"></div>
-                            <input type="email" class="form-control" id="email" placeholder="email">
-                            <small></small>
-                        </div>
-                        <div class=" mb-3">
+                            <input type="checkbox" name="Email Marketing" value="Marketing" id="check"> Email Marketing
                             <div class="pt-3"></div>
-                            <input type="text" class="form-control" id="telefono" onkeypress="return valideKey(event);" placeholder="Telefono">
-                            <small></small>
-                        </div>
-                        <div class=" mb-3">
+                            <input type="checkbox" name="Email Marketing" value="Marketing" id="check"> News & Updates Emails
                             <div class="pt-3"></div>
-                            <textarea name="mensajes" class="me" id="mensaje" cols="30" rows="4" placeholder="Mensaje"></textarea>
-                            <small></small>
+                            <input type="checkbox" name="Email Marketing" value="Marketing" id="check"> Production Process Emails
                         </div>
                         <button type="submit">Enviar</button>
-                        <div class="pt-3"></div>
-                    </form>
+                        
+                </form>
 
                 </div>
-
-
-
-                <div class="pt-3"></div>
             </div>
         </div>
-    </div>
+    
     <div class="pt-5"></div>
     <?php include 'footer.html' ?>
 </body>
