a34da48bbbf0135b786460f7cb4d7a0ceb2f8de5
Jennykings <jennykings51@gmail.com>
2023-03-10 14:53:45 -0500

    añadiendo servicios

--- View/servicios.php
+++ View/servicios.php
@@ -6,21 +6,121 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>.:: DigiMedia ::.</title>
-    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <!-- Fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="css/stylos.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/stylos.css">
+    <link rel="stylesheet" href="../css/servicios.css?secwc">
+
+
+
 </head>
 
 <body>
     <?php include 'header.html' ?>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+   <div class="banner">
+        <div class="principal">
+            <div class="letra_titulo">
+                <h1 class="d">DIGIMEDIA</h1>
+                <p class="creativas">Innovadora y Creativa</p>
+            </div>
+            <div class="info">
+            <p class="es" style="color:#FF0080;">Estamos enfocados a lograr <span style="color:#9e7fe2;"> resultados comerciales</span> y de <span style="color:#9e7fe2;" >marketing</span>  con <span style="color:#9e7fe2;">creatividad</span>  y <span style="color:#9e7fe2;">estrategias</span></p>
+            </div>
+            <div class="btu">
+                <a href="contacto.php"><button href="contacto.php" class="con ">Contactar</button></a>
+            </div>
+        </div>
+   </div>
+
+   <div class="">
+        <div class="ser">
+        <h4>Servicios de Marketing</h4>
+        </div>
+        <div class="co">
+            <div class="mar">
+
+                <a href="#" class="hover">
+                    <p>Marketing Digital</p>
+                    <img src="../img/md.webp" alt="">
+                </a>
+                <a class="hover">
+                    <p>Imagen Corporativa</p>
+                    <img src="../img/cor.webp" alt="">
+                </a>
+                <a class="hover">
+                    <p>Gesti&oacute; de Redes Sociales</p>
+                    <img src="../img/so.webp" alt="">
+                </a>
+            </div>
+        </div>
+   </div>
+
+   <div class="">
+        <div class="ser">
+        <h4>Servicios para Tu marca</h4>
+        </div>
+        <div class="cx">
+            <div class="mar">
+            <a href="#" class="hover">
+                    <p>Diseño Web</p>
+                    <img src="../img/dw.webp" alt="">
+                </a>
+                <a class="hover">
+                    <p>Crear tu Marca</p>
+                    <img src="../img/ct.webp" alt="">
+                </a>
+                <a class="hover">
+                    <p>Diseño Grafico</p>
+                    <img src="../img/dg.webp" alt="">
+                </a>
+            </div>
+        </div>
+   </div>
+
+   <div class="pt-4"></div>
+   <div class="cp">
+    <div class="tx">
+        <div class="texg">
+            <h3 class="no">¿No sabes por d&oacute;nde empezar?</h3><div class="pt-3"></div>
+            <p class="parr">En Digimedia, te brindamos asesor&iacute;a perssonalizada para emprender o hacer crecer tu negocio.</p>
+            <div class="btr">
+            <a href="" ><button class="b">Contactanos</button></a>
+            </div>
+        </div>
+    </div>
+   </div>
+
+   <div class="pt-4"></div>
+   <div class="cpx">
+    <div class="tx2">
+        <div class="sssa">
+            <h3 class="no2">¿Tienes alguna duda sobre nuestros servicios?</h3><div class="pt-4"></div>
+            <p class="texte2r">¡Tenemos la respuesta!</p>
+            <p class="parr2">Echa un vistazo a nuestra secci&oacute;n de <span style="font-weight: bold;">Preguntas Frecuentes</span> donde encontrar&aacute;s respuestas a las preguntas m&acute;s comunes.</p>
+            <p class="saxc2">
+                Haz clic en el bot&oacute;n "Ver Preguntas Frecuentes" para acceder  a esta informaci&oacute;n valiosa.
+            </p>
+            <div class="btr2">
+            <a href="" ><button class="b">Preguntas Frecuentes</button></a>
+            </div>
+        </div>
+    </div>
+   </div>
+
+    <div class="pt-5"></div>
     <?php include 'footer.html' ?>
+
+
 </body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<!-- JS propio -->
+<!--<script defer src="https://app.embed.im/snow.js"></script>
+<script src="js/main.js"></script>-->
+
+<script src="../js/maquina.js"></script>
 
 </html>
