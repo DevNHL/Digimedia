aade6aac30b9f1de7b39db5fe275e40ae0bf2716
Jeam06 <jesusangeldonismaslucan@gmail.com>
2023-03-11 13:37:58 -0500

    enviar datos a DB

--- View/insertarcontacto.php
+++ View/insertarcontacto.php
@@ -1,5 +1,16 @@
 <?php
-    include "../core/conexion.php";
+    // Datos para la conexión
+  $server = 'localhost';
+  $user = 'root';
+  $pass = '';
+  $db = 'digimedia';
+
+  // Conexión a bd - MySQL
+  $conn = mysqli_connect($server, $user, $pass, $db);
+
+   //if(isset($conn)){
+   //  echo 'DB is connect';
+   //}
 
     $nom=$_POST['nombre'];
     $em=$_POST['email'];
@@ -9,6 +20,17 @@
     $emM=$_POST['emailMarck'];
     $new=$_POST['new'];
     $pro=$_POST['production'];
-    $sql = "INSERT INTO 'contactanos' ( nombre , email , servicio , numero , mensaje , emailMarck , new , production)
-            VALUES ('$nom','$em','$ser','$num','$men','$emM','$new','$pro')";
+    $sql = "INSERT INTO `contactanos` (`id`, `nombre`, `email`, `servicio`, `numero`, `mensaje`, `emailMarck`, `new`, `production`) 
+            VALUES (NULL, '$nom', '$em', '$ser', '$num', '$men', '$emM', '$new', '$pro')";
+    $resultado = mysqli_query($conn, $sql);
+
+    // Comprobar si los datos se han insertado correctamente
+    if ($resultado) {
+        echo "Los datos se han insertado correctamente";
+    } else {
+        echo "Ha habido un error al insertar los datos: " . mysqli_error($conn);
+    }
+
+    // Cerrar la conexión con la base de datos
+    mysqli_close($conn);
 ?>
