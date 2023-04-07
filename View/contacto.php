f5c4f5618039edbabc1517018e8f04a44d3424fe
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-04-07 17:30:27 -0500

    cam

--- View/contacto.php
+++ View/contacto.php
@@ -10,9 +10,8 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="../css/stylos.css?130220">
+    <link rel="stylesheet" href="../css/conctato.css">
     <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/validar.css">
     
 </head>
 
