5877f0b078fe5d2177e376de4cd7595d7ada32d4
Jennykings <jennykings51@gmail.com>
2023-04-18 12:36:22 -0500

    añadiendo

--- View/contacto.php
+++ View/contacto.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -8,91 +8,90 @@
     <title>.:: DigiMedia ::.</title>
     <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
     <!-- CSS only -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="../css/stylos.css?130220">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/validar.css">
-    
+    <link rel="stylesheet" href="../css/conctato.css?2000">
 </head>
 
 <body>
     <?php include 'header.html' ?>
-    <div class="contact" id="banner">
-        <div class="sombra">
-        </div>
-        <div class="letra" id="">
-            <div class="borde" id="digimedia">
-                <p class="" id=""><u class="delgado">CONTÁCTANOS</u></p>
+
+    <center >
+        <h4 class="h1">Contactanos</h4>
+    </center>
+    <div class="principal">
+        <div class="pos">
+            <div class="imagen">
+                
+                <h4 class="h1x">Contactanos</h4>
             </div>
-        </div>
-    </div>
-        <div class="pt-5"></div>
-            <div class="container">
+            <div class="formulario">
                 <div class="col-md-10 offset-md-1">
-                <form action="insertarcontacto.php" method="post" id="formulario">
-                    <input type="text" name="fecha" id="fecha"  style="display: none;">
-                    <input type="text"  name="hora" id="hora" style="display: none;">
-                    <?php
-                    # si hay un mensaje, mostrarlo
-                    if (isset($_GET["mensaje"])) { ?>
-                        <div class="alert alert-warning alert-dismissible fade show des" role="alert">
-                            <?php echo $_GET["mensaje"] ?>
-                        </div>
-                    <?php } ?>
-                    <div class="row">    
-                        <div class="col-md-6">
-                        <div class="mb-3">
-                            <div class="pt-3">Nombre</div>
-                            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombres y Apellidos" required>
-                            <small></small>
-                        </div>
+                    <form action="insertarcontacto.php" method="post" id="formulario">
+                        <input type="text" name="fecha" id="fecha" style="display: none;">
+                        <input type="text" name="hora" id="hora" style="display: none;">
+                        <?php
+                        # si hay un mensaje, mostrarlo
+                        if (isset($_GET["mensaje"])) { ?>
+                            <div class="alert alert-warning alert-dismissible fade show des" role="alert">
+                                <?php echo $_GET["mensaje"] ?>
+                            </div>
+                        <?php } ?>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <div class="pt-3">Nombre</div>
+                                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombres y Apellidos" required>
+                                    <small></small>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <div class="pt-3">Email</div>
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="email" required>
+                                    <small></small>
+                                </div>
+                            </div>
                         </div>
 
-                        <div class="col-md-6">
-                        <div class="mb-3">
-                            <div class="pt-3">Email</div>
-                            <input type="email" class="form-control" name="email" id="email" placeholder="email" required>
-                            <small></small>
-                        </div>
-                        </div>
-                    </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <div class="pt-3">Servicio</div>
+                                    <select class="form-control" name="servicio" id="servicio" required>
+                                        <option selected disabled value="">--Selecciona--</option>
+                                        <option value="Marketing Digital">Marketing Digital</option>
+                                        <option value="Imagen Corporativa">Imagen Corporativa</option>
+                                        <option value="Gestión De Redes Sociales">Gestión De Redes Sociales</option>
+                                        <option value="Crea Tu Marca">Crea Tu Marca</option>
+                                        <option value="Diseño web">Diseño web</option>
+                                        <option value="Diseño Grafico">Diseño Grafico</option>
+                                    </select>
+                                    <small></small>
+                                </div>
+                            </div>
 
-                    <div class="row">
-                        <div class="col-md-6">
-                        <div class="mb-3">
-                            <div class="pt-3">Servicio</div>
-                            <select class="form-control" name="servicio" id="servicio" required>
-                                <option selected disabled value="">--Selecciona--</option>
-                                <option value="Marketing Digital">Marketing Digital</option>
-                                <option value="Imagen Corporativa">Imagen Corporativa</option>
-                                <option value="Gestión De Redes Sociales">Gestión De Redes Sociales</option>
-                                <option value="Crea Tu Marca">Crea Tu Marca</option>
-                                <option value="Diseño web">Diseño web</option>
-                                <option value="Diseño Grafico">Diseño Grafico</option>
-                            </select>
-                            <small></small>
-                        </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <div class="pt-3">Numero</div>
+                                    <input type="text" class="form-control" name="numero" id="numero" onkeypress="return valideKey(event);" placeholder="Telefono(9 dígitos)" required>
+                                    <small></small>
+                                </div>
+                            </div>
                         </div>
 
-                        <div class="col-md-6">
-                        <div class="mb-3">
-                            <div class="pt-3">Numero</div>
-                            <input type="text" class="form-control" name="numero" id="numero" onkeypress="return valideKey(event);" placeholder="Telefono(9 dígitos)"required >
-                            <small></small>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="pt-5"></div>
 
                         <div class="mb-3">
                             <div class="pt-3">Mensaje</div>
                             <textarea name="mensaje" class="form-control" id="mensaje" cols="30" rows="4" placeholder="Mensaje" required></textarea>
                             <small></small>
                         </div>
-                        
-                        <div class="mb-3">
-                            <div class="pt-5">Opt-in</div>
+
+                        <div class="mb-3" id="con">
+                            <div class="pt-3 w-100">Opt-in</div>
+                            <div class="divs"></div>
+                            <div class="divs2"></div>
                             <input type="hidden" name="emailMarck" value="no" id="email">
                             <input type="checkbox" name="emailMarck" value="si" id="email"> Email Marketing
                             <div class="pt-3"></div>
@@ -103,18 +102,50 @@
                             <input type="checkbox" name="production" value="si" id="production"> Production Process Emails
                         </div>
 
-                        <div class="pt-5"></div>
-                        <button type="submit">Enviar</button>
+                        <div class="pt-3" id="ps"></div>
+                        <input type="submit" value="Enviar" class="enviar">
+                        <div class="links">
+                            <a href="" class="f">Facebook</a>
+                            <a href="" class="i">Instagram</a>
+                            <a href="" class="l">Linkedin</a>
+
                         </div>
-                </form>
+
+
+
+
+                    </form>
 
                 </div>
             </div>
         </div>
-        
-    <div class="pt-5"></div>
-    
-    <?php include 'footer.html' ?>
+        <div class="col">
+           
+            <div class="cont">
+                <div class="grande">
+                    <h2 style="font-weight: bold;">Openig Hours</h2>
+                </div>
+                <div>
+                    <p>Mon-Fri</p>
+                    <p>Saturday</p>
+                    <p>Sunday</p>
+                </div>
+                <div>
+                    <p>8:00 am - 8:00 pm</p>
+                    <p>9:00 am - 7:00 pm</p>
+                    <p>9:00 am - 9:00 pm</p>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="mapa">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d126777.96020960424!2d-79.72782079999999!3d-6.7928064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2spe!4v1681256123713!5m2!1ses!2spe"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+        </div>
+        <div>
+
+            <?php include 'footer.html' ?>
+
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 <script type="text/javascript">
@@ -138,5 +169,6 @@ $(document).ready(function() {
 
 
 </script>
+
 
 </html>
