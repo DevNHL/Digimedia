9826d424542f5fe08149ae1f7556a041fe8cba98
Jennykings <jennykings51@gmail.com>
2023-04-18 12:06:32 -0500

    añadiendo

--- intranet.php
+++ intranet.php
@@ -1,5 +1,12 @@
 <?php 
-  
+//seguridad de sesiones
+session_start();
+error_reporting(0);
+$varsesion=$_SESSION['usuario'];
+if($varsesion==null || $varsesion=''){
+    header("location: admin/login/index.php");
+    die();
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,17 +19,10 @@
     <title>.:: DigiMedia ::.</title>
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/stylos.css?48588">
+    <link rel="stylesheet" href="public/css/consultas.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js"></script>
-
-    <!--    Scripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.js" integrity="sha512-sk0cNQsixYVuaLJRG0a/KRJo9KBkwTDqr+/V94YrifZ6qi8+OO3iJEoHi0LvcTVv1HaBbbIvpx+MCjOuLVnwKg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
 
 </head>
 
@@ -30,21 +30,15 @@
     
     <br>
     <h1 class="text-center">Contactos Lista</h1>
-   
-    
 
-    <div class="cen">
-    <a type="button" class="btn btn-primary " href="admin/cerrar.php" id="cer">Cerrar Sesion</a>
+    <center>
+    <a type="button" class="btn btn-primary" href="admin/logout.php">Cerrar Sesion</a>
     <a type="button" class="btn btn-primary" href="admin/usuarios/index.php">Lista Usuarios</a>
-   
-    </div>
-   
-
-
+    </center>
     <br>
 
-    <div class="table-responsive px-5 " id="sx">
-    <table class="table table-striped table-bordered" id="miTabla">
+    <div class="table-responsive px-5 ">
+    <table class="table table-striped table-bordered">
       <thead>
         <tr>
             <th scope="col">ID</th>
@@ -56,8 +50,6 @@
             <th scope="col">EMAIL MARK</th>
             <th scope="col">NEW</th>
             <th scope="col">PRODUCTION</th>
-            <th scope="col">Fecha</th>
-            <th scope="col">Hora</th>
             <th scope="col">ACCION</th>
         </tr>
       </thead>
@@ -111,16 +103,13 @@
                         <td><?php echo $data['emailMarck'] ?></td>
                         <td><?php echo $data['new'] ?></td>
                         <td><?php echo $data['production'] ?></td>
-                        <td><?php echo $data['fecha'] ?></td>
-                        <td><?php echo $data['hora'] ?></td>
 
-                        <td style="text-align: center" id="sx">
+                        <td style="text-align: center">
                             <form action="View/atendido.php" method="POST">
                                 <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
-                                <button type="submit" class="btn btn-danger" onclick="return confirm('&iquest Esta seguro de que desea dar por atendido?');">Atendido</button>
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('¿Está seguro de que desea dar por atendido?');">Atendido</button>
                             </form>
                         </td>
-
                     </tr>
             <?php
                 }
@@ -128,18 +117,9 @@
         </tbody>
     </table>
     </div>
-    <div class="cen">
-    <button onclick="exportToExcel()">Exportar a Excel</button>
-    <button  id="pdfout">Generar PDF</button>
-    <button onclick="imprimirParteDePagina()">Imprimir</button>
-    </div>
     </main>
-    
-     
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-<script src="js/export.js"></script>
-<script src="js/pdf.js"></script>
-<script src="js/impresion.js"></script>
 
 </html>
