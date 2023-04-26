724e9a124fcb12cdc54ed7f75f3e2bcad252faf3
Jennykings <jennykings51@gmail.com>
2023-04-26 00:03:30 -0500

    añadiendo cambios

--- intranet.php
+++ intranet.php
@@ -32,15 +32,32 @@ if (!isset($_SESSION['usuario'])) {
     background-color: red;
     border: none;
     color: white;
-    padding: 8px;
-    width:150px;
+    padding: 6px;
+    width:40px;
     border-radius: 6px;
+    margin-left: 10px;
     
    }
-
+   .señ{
+    
+    background-color: greenyellow;
+    padding: 5px;
+    border-radius: 20px;
+    font-size: 11px;
+    color: black;
+    font-weight: bold;
+    
+   }
+   .at{
+    background-color: red;
+    padding: 5px;
+    border-radius: 20px;
+    font-size: 11px;
+    color: white;
+    font-weight: bold;
+   }
 
 </style>
-
 
 </head>
 
@@ -54,11 +71,19 @@ if (!isset($_SESSION['usuario'])) {
     </div>
    
 
-
     <br>
 
     <div class="table-responsive px-5 " id="sx">
+   
     <table class="table table-striped table-bordered" id="miTabla">
+    <?php
+        # si hay un mensaje, mostrarlo
+        if (isset($_GET["mensaje"])) { ?>
+           <div class="alert alert-primary des" role="alert">
+                <?php echo $_GET["mensaje"] ?>
+            </div>
+    <?php } ?>
+
       <thead>
         <tr>
             <th scope="col">ID</th>
@@ -72,6 +97,7 @@ if (!isset($_SESSION['usuario'])) {
             <th scope="col">PRODUCTION</th>
             <th scope="col">Fecha</th>
             <th scope="col">Hora</th>
+            <th scope="col">ESTADO</th>
             <th scope="col">ACCION</th>
         </tr>
       </thead>
@@ -87,25 +113,11 @@ if (!isset($_SESSION['usuario'])) {
             // Conexión a bd - MySQL
             $conn = mysqli_connect($server, $user, $pass, $db);
 
-            //Paginador
-            $sql_registe = mysqli_query($conn, "SELECT COUNT(*) as total_registro FROM contactanos");
-            $result_register = mysqli_fetch_array($sql_registe);
-            $total_registro = $result_register['total_registro'];
+            
+            
+           
 
-            $por_pagina = 50;
-
-            if (empty($_GET['pagina'])) {
-                $pagina = 1;
-            } else {
-                $pagina = $_GET['pagina'];
-            }
-
-            $desde = ($pagina - 1) * $por_pagina;
-            $total_paginas = ceil($total_registro / $por_pagina);
-
-            $query = mysqli_query($conn, "SELECT * FROM contactanos 
-                                     ORDER BY id 
-                                     ASC LIMIT $desde,$por_pagina");
+            $query = mysqli_query($conn, "SELECT * FROM contactanos  ORDER BY estado DESC; ");
 
             mysqli_close($conn);
 
@@ -113,6 +125,11 @@ if (!isset($_SESSION['usuario'])) {
             if ($result > 0) {
 
                 while ($data = mysqli_fetch_array($query)) {
+                if ($data['estado'] == 1) {
+                    $estado = '<span class="señ">Pendiente</span>';
+                } else {
+                    $estado = '<span class="at">Atendido</span>';
+                }
             ?>
                     <tr>
 
@@ -134,10 +151,15 @@ if (!isset($_SESSION['usuario'])) {
                                 <button type="submit" class="btn btn-danger" onclick="return confirm('&iquest Esta seguro de que desea dar por atendido?');">Atendido</button>
                             </form>
                         </td>-->
+                        <td><?php echo $estado ?></td>
+                        <?php if ($data['estado'] == 1) { ?>
                         <td>
-                           <button class="btnsx pendiente ">Pendiente</button>
+                           <form action="View/atendido.php" method="POST">
+                                 <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
+                               <button type="submit" class="btnsx pendiente " onclick="return confirm('&iquest Esta seguro de que desea dar por atendido?');"><i class="fa-solid fa-trash"></i></button>
+                           </form>
                         </td>
-
+                        <?php } ?>
                     </tr>
             <?php
                 }
@@ -162,7 +184,7 @@ if (!isset($_SESSION['usuario'])) {
 
 <script>
     
-    var bton=document.getElementsByClassName("pendiente");
+   /* var bton=document.getElementsByClassName("pendiente");
    
     
     
@@ -174,21 +196,22 @@ if (!isset($_SESSION['usuario'])) {
           this.style.backgroundColor = "orange";
           this.innerText="Atendido";
           
-          //localStorage.setItem('colorBoton', this.style.backgroundColor);
+         
         };
         
-        /*const colorGuardado = localStorage.getItem('colorBoton');
-       // console.log(colorGuardado)
-        if (!colorGuardado) {
-          
-        }else{
-            //this.style.backgroundColor = colorGuardado;
-            bton[i].style.backgroundColor=colorGuardado;
-            console.log(bton[i].innerHTML)
-           
-       }*/
+      
         
-    }
+    }*/
       
 </script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      setTimeout(function() {
+        $(".des").fadeOut(1500);
+      }, 6000);
+    });
+  </script>
+
 </html>
