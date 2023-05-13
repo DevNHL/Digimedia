248eaada82627b3be9326b29e8c777767af0f05f
Jennykings <jennykings51@gmail.com>
2023-05-12 20:04:56 -0500

    añadiendo nuevos cambios

--- View/ds-web.php
+++ View/ds-web.php
@@ -13,6 +13,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
     <link rel="stylesheet" href="../css/diseno_web.css">
+    <!-- Stylos Css para ventana emergente(Prueba 1) -->
+    <link rel="stylesheet" type="text/css" href="../css/vent_emrgnt.css">
     
     
 
@@ -92,7 +94,50 @@
             
 
            <?php include 'footer.html' ?>
+           <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-4">
 
+                <!--====Solicitud Pop-up====-->
+                <div class="registration-form">
+                    <h5 class="text-center">Brindanos tus datos</h5>
+
+                    <p class="text-success text-center"></p> <form action="" method="post">
+
+                    <!--//Nombre//-->
+                    <div class="form-group">
+                    <label for="email">Nombre</label>
+
+                        <input type="text" class="form-control" placeholder="Ingresar Nombre Completo" name="first_name" value="">
+
+                    <p class="err-msg">
+                    </p>
+                    </div>
+                    <!--//Telefono//-->
+                    <div class="form-group">
+                        <label for="email">Telefono</label>
+
+                        <input type="text" class="form-control" placeholder="Ingresar Telefono Personal" name="last_name" value="">
+                        <p class="err-msg"> 
+
+                        </p>
+                    </div>
+
+                    <!--//Correo//-->
+                    <div class="form-group">
+                        <label for="email">Correo:</label>
+
+                    <input type="text" class="form-control" id="email" placeholder="Ingresar Correo" name="email" value="">
+                        <p class="err-msg">
+                        </p>
+                    </div>
+
+                    <button type="submit" class="btn btn-danger" name="submit" style="margin: 10px;">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 <script type="text/javascript">
