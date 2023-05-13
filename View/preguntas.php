38aaa3443a865f6dc5eb67709453b93c41a63414
Jennykings <jennykings51@gmail.com>
2023-05-12 20:13:07 -0500

    añadiendo nuevos cambios

--- View/preguntas.php
+++ View/preguntas.php
@@ -13,7 +13,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Stylos Css propios -->
-    <link rel="stylesheet" href="../css/stylos.css">
+    <link rel="stylesheet" href="../css/preguntas.css">
     <link rel="stylesheet" href="../css/header.css?150823">
 
 </head>
